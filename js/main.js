ac3a47b156097c06c6502955de1e3e4575ae516b
Gin Sheng <tsen.tso@gmail.com>
2014-05-22 18:07:56 -0500

    Island added, and more...
    Now we have changing wind conditions, every 5 seconds.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 GAME_FPS = 30;
 GAME_WIDTH = 480;
 GAME_HEIGHT = 320;
+WEATHER_PROBABILITIES = 5; // seconds to change wind direction by first time
 paused = false;
+nautical_rose = ['north', 'south', 'east', 'west'];
 function start() {
 	var gameField = document.getElementById('gameField');
 
@@ -18,10 +20,16 @@ function start() {
 	SEA_HEIGHT = GAME_HEIGHT;
 
 	// creating wind
-	wind = new Wind({_speed: 1.7, _direction: 'east'});
+	wind = new Wind({_speed: 1.3, _direction: 'east'});
 
 	// creating boat
 	boat = new Boat({_height: 20, _width: 20});
+
+	// creating an island
+	var randomX = Math.floor((Math.random() * SEA_WIDTH) - 35);
+	var randomY = Math.floor((Math.random() * SEA_HEIGHT) + 35);
+	// console.debug('Coordenadas de la isla: ' + randomX + ', ' + randomX);
+	island = new Island({_height: 35, _width: 35, _x: randomX, _y: randomY});
 
 	// adding the keyboard listener
 	set_ketListener();
@@ -34,12 +42,16 @@ function game_action() {
 		game_beat = setInterval(function(){
 			draw(universe.getContext('2d'));
 		}, 1000/GAME_FPS);
+		wind_conditions = setInterval(function(){
+			change_wind();
+		}, 1000 * WEATHER_PROBABILITIES);
 }
 
 function draw(context) {
 	context.clearRect(0, 0, SEA_WIDTH, SEA_HEIGHT);
 	boat.move(wind);
 	boat.draw(context);
+	island.draw(context);
 }
 
 function set_ketListener() {
@@ -73,8 +85,19 @@ function set_ketListener() {
 function toggle_pause() {
 	paused = !paused;
 
-	if (paused)
+	if (paused) {
 		clearInterval(game_beat);
+		clearInterval(wind_conditions);
+	}
 	else
 		game_action();
+}
+
+function change_wind() {
+	// clearInterval(wind_conditions);
+	var i = Math.floor(Math.random() * 4);
+	WEATHER_PROBABILITIES = Math.floor((Math.random() * 12) + 6);
+	console.log('Cambia el viento: ' + nautical_rose[i]);
+	wind.change({_speed: wind._speed, _direction: nautical_rose[i]} );
+	// wind_conditions = setInterval(change_wind(), 1000 * WEATHER_PROBABILITIES);
 }
